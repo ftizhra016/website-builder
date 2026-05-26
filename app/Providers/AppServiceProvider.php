63595e8a3a30f23cb7d920dfcce1b2f550ce3a6c
Fatimah Azzahra <youecan016@gmail.com>
2026-05-26 16:49:05 +0700

    chore: triggers complete rebuild code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL; // Jangan lupa tambahkan baris ini!
+use Illuminate\Support\Facades\URL; // Force rebuild server Linux
 
 class AppServiceProvider extends ServiceProvider
 {
